b79a8b5b6ca6d15b99c0e7f50071648308369376
skywang1234 <32024922+skywang1234@users.noreply.github.com>
2024-08-31 23:50:45 -0400

    first commit

---
+++ NeetCode150/ContainsDuplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        unique = set()
+        for n in nums:
+            if n in unique:
+                return True
+            else:
+                unique.add(n)
+        return False
+        
