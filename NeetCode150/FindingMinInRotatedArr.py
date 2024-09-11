746b7e53bec9a7b9ae3fbfb9c8d9a29649b4714b
skywang1234 <32024922+skywang1234@users.noreply.github.com>
2024-09-11 17:52:55 -0400

    add Finding Min In Rotated Arr solution

--- NeetCode150/FindingMinInRotatedArr.py
+++ NeetCode150/FindingMinInRotatedArr.py
@@ -1,16 +1,11 @@
-nums = [11,13,15,17]
-left = 0
-right = len(nums)-1
-#in correct sequence left>mid<right
-while left<right:
-    mid = left+(right-left)//2
-    if nums[mid]>nums[left]:
-        right = mid-1
-        continue
-    elif nums[mid]>nums[right]:
-        left = mid+1
-        continue
-    else:
-        print(nums[mid])
-        break
-print(nums[mid])
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums)-1
+        while left < right:
+            mid = (left+right)// 2
+            if nums[mid]<nums[right]:
+                right = mid
+            else:
+                left = mid+1    
+        return nums[left]
