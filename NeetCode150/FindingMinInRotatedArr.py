c91b287c5eda29a550cd4e8af2af4c9bfeb8b64f
skywang1234 <32024922+skywang1234@users.noreply.github.com>
2024-09-11 17:59:26 -0400

    fix minor formatting error in Rotated Arr solution

--- NeetCode150/FindingMinInRotatedArr.py
+++ NeetCode150/FindingMinInRotatedArr.py
@@ -3,7 +3,7 @@ class Solution:
         left = 0
         right = len(nums)-1
         while left < right:
-            mid = (left+right)// 2
+            mid = (left+right)//2
             if nums[mid]<nums[right]:
                 right = mid
             else:
